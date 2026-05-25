fe6eba4440fdec3f2cdf53a6b6d8e5db46f076e3
xcutoboo <iamnumberone751@gmail.com>
2026-05-25 16:18:42 -0600

    publish flow: jitpack, hangar, modrinth, polymart, release-please
    Wires up the marketplace pipeline. mythicrod-api goes out
    through JitPack. Hangar uses the hangar-publish-plugin pointing
    at the shadow jar. Modrinth uploads via mc-publish and the body
    gets synced from README. Polymart posts to api.voxel.shop with a
    multipart form. Release Please tracks the changelog and the
    gradle.properties version marker so the next bump is mechanical.
    Pages workflow now pins exact mkdocs versions.

--- mythicrod-api/build.gradle.kts
+++ mythicrod-api/build.gradle.kts
@@ -1,5 +1,43 @@
+plugins {
+    `maven-publish`
+}
+
 dependencies {
     compileOnly(libs.jetbrains.annotations)
 
     testImplementation(libs.junit.jupiter)
+    testCompileOnly(libs.jetbrains.annotations)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            from(components["java"])
+            groupId = "io.xcutiboo"
+            artifactId = "mythicrod-api"
+            version = project.version.toString()
+
+            pom {
+                name.set("MythicRod API")
+                description.set("Public integration surface for MythicRod (Paper / Folia).")
+                url.set("https://github.com/xcutiboo/MythicRod")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://github.com/xcutiboo/MythicRod/blob/master/LICENSE")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/xcutiboo/MythicRod")
+                    connection.set("scm:git:git://github.com/xcutiboo/MythicRod.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:xcutiboo/MythicRod.git")
+                }
+            }
+        }
+    }
 }
